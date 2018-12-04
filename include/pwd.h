3d76aacee798c721df73ffa36fff37ba5aad507c
Davit Kalantaryan <davit.kalantaryan1980@gmail.com>
2018-12-04 13:28:22 +0100

    some headers updated from udoocs

--- include/pwd.h
+++ include/pwd.h
@@ -30,6 +30,7 @@ GEM_API_FAR int getpwnam_r(const char *name, struct passwd *pwd,
 	char *buf, size_t buflen, struct passwd **result);
 GEM_API_FAR struct passwd *getpwnam(const char *name);
 GEM_API_FAR struct passwd *getpwuid(uid_t uid);
+GEM_API_FAR int getpwuid_r(uid_t uid, struct passwd *pwd,char *buf, size_t buflen, struct passwd **result);
 
 __END_C_DECLS
 
