71a5c7c808e9fcb6678029ffb3ca258d10c11fa0
Davit Kalantaryan <davit.kalantaryan@desy.de>
2019-01-26 19:37:02 +0100

    modified a bit termios.h

--- include/termios.h
+++ include/termios.h
@@ -9,5 +9,19 @@
 #ifndef __wlac_termios_h__
 #define __wlac_termios_h__
 
+#include <first_includes/common_include_for_headers.h>
+
+__BEGIN_C_DECLS
+
+struct winsize
+{
+	unsigned short int ws_row;
+	unsigned short int ws_col;
+	unsigned short int ws_xpixel;	/*unused*/
+	unsigned short int ws_ypixel;	/*unused*/
+};
+
+__END_C_DECLS
+
 
 #endif // #ifndef __wlac_termios_h__
